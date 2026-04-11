a6c85a148f84e854100ea9e5cad998b1e07cc6ab
Olukayode <theolukayodeasemudara@gmail.com>
2026-04-11 09:10:53 +0100

    re-adding chapter-two folder

---
+++ ChapterTwo/num_six.py
@@ -0,0 +1,3 @@
+grade = int(input("Enter your score! "))
+if grade >= 90:
+    print('Congratulations! Your grade of', grade, 'earns you an A in this course')
---
+++ ChapterTwo/number_five.py
@@ -0,0 +1,3 @@
+rating = input('Enter an integer rating between 1 and 10: ')
+
+print("Inputs by default converts any value that it accepts into strings")
