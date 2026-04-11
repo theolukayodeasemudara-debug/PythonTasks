9d89f3ffb79ced4d7cb923ac2136da0b4345d67b
Olukayode <theolukayodeasemudara@gmail.com>
2026-04-11 18:49:19 +0100

    Fix upload

---
+++ Assignments/Chapter2naBeans/exercise5.py
@@ -0,0 +1,2 @@
+average = (10 + 20 + 30 + 40 + 50) / 5
+print(average)
---
+++ Assignments/Chapter2naBeans/exercise7.py
@@ -0,0 +1,12 @@
+grade = input("Enter a grade (A, B, C, D, F): ")
+
+if grade == "A":
+    print("90-100")
+elif grade == "B":
+    print("80-89")
+elif grade == "C":
+    print("70-79")
+elif grade == "D":
+    print("60-69")
+elif grade == "F":
+    print("below 60")
