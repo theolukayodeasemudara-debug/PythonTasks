b7e33168791152350283a8d4d4c508f05e9343d2
Olukayode <theolukayodeasemudara@gmail.com>
2026-04-12 14:23:47 +0100

    first commit

---
+++ Assignments/mortgageCalculator/mortgagecalc.py
@@ -0,0 +1,7 @@
+# collect and convert user's input to a number
+loan_amount = int(input("How much do you wish to borrow? "))
+loan_duration = int(input("What is your preferred duration? "))
+
+# check the input type
+print(type(loan_amount))
+
