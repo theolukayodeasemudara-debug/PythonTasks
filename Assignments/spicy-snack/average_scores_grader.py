4a5463735e20764bc4984232af601e949a3031e4
Olukayode <theolukayodeasemudara@gmail.com>
2026-05-09 10:47:00 +0100

    collectng three scores successfully

---
+++ Assignments/spicy-snack/average_scores_grader.py
@@ -0,0 +1,7 @@
+# ask user to enter a score between 0 to 100
+# collect three scores from my terminal
+# find the average of the three scores collected
+# check if score is less than different threshold then return a letter grade
+
+for i in range(3):
+    score = input("enter your score: ")
